e5091362e61083b7c3d14e2d6e07d5df6fdcaf1a
Aradhya <aradhyatripathi51@gmail.com>
2021-11-21 22:44:06 +0530

    feat: forbidding extra fields in contactus schema

--- server/api/definitions.py
+++ server/api/definitions.py
@@ -2,6 +2,9 @@ from pydantic import BaseModel, constr
 
 
 class ContactUsSchema(BaseModel):
+    class Config:
+        extra = "forbid"
+
     name: str
     email: constr(regex="(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
     query: constr(min_length=30, strip_whitespace=True)
