bc6bba04b1fb7b9de1668ecb869a587829ffdf57
Aradhya <aradhyatripathi51@gmail.com>
2021-11-21 23:31:18 +0530

    feat: Added deployment files

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 from server.api.view import router as api_view
 from server.api.exception_handlers import invalid_data_handler
 from server.api.limiter import limiter
@@ -7,6 +8,14 @@ from server.api.limiter import limiter
 __version__ = "0.1.0"
 
 application = FastAPI()
+
+application.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 application.state.limiter = limiter
 application.exception_handler(RequestValidationError)(invalid_data_handler)
