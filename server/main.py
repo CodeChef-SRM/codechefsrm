dd95e7cef087518b6647ef57577cf8a771e3b4c5
Aradhya <aradhyatripathi51@gmail.com>
2021-11-23 01:20:33 +0530

    refactor: moved health-check to main namespace

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from server.api.view import router as api_view
 from server.api.exception_handlers import invalid_data_handler
 from server.api.limiter import limiter
+from server.api.utils import process_time
 
 __version__ = "0.1.0"
 
@@ -20,3 +21,9 @@ application.add_middleware(
 application.state.limiter = limiter
 application.exception_handler(RequestValidationError)(invalid_data_handler)
 application.include_router(api_view, prefix="/api")
+
+
+@application.get("/health-check", status_code=200)
+async def health_check():
+    uptime = await process_time()
+    return {"upTime": uptime}
