91fa8e56ec2325f97133217aa38f6346187f7184
Aradhya <aradhyatripathi51@gmail.com>
2021-11-24 23:49:11 +0530

    feat: Added schema for admin register and login

--- server/api/definitions.py
+++ server/api/definitions.py
@@ -10,10 +10,19 @@ class ContactUsSchema(BaseModel):
     query: constr(min_length=30, strip_whitespace=True)
 
 
-class AdminRegister(BaseModel):
+class AdminRegisterSchema(BaseModel):
     class Config:
         extra = "forbid"
 
     name: str
     email: constr(regex="(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+    password: constr(min_length=8)
     web_hook: constr(strip_whitespace=True)
+
+
+class AdminLoginSchema(BaseModel):
+    class Config:
+        extra = "forbid"
+
+    email: constr(regex="(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+    password: str
