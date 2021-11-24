2d64e892e3cfb7ed38ef2394908183b03b1ed72b
Aradhya <aradhyatripathi51@gmail.com>
2021-11-24 22:20:44 +0530

    feat: Added admin insertion in models

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -4,4 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = Model(mongo_uri=os.getenv("mongo_uri"), database=os.getenv("database"))
+model = Model(
+    mongo_uri=os.getenv("mongo_uri"),
+    database=os.getenv("database"),
+    web_hook=os.getenv("web_hook"),
+)
