a93f7c4de7063f22d3946eed81219f692e7c5a01
Aradhya <aradhyatripathi51@gmail.com>
2021-11-24 22:21:28 +0530

    🥅 Added duplication errors

--- server/errors/__init__.py
+++ server/errors/__init__.py
@@ -1,1 +1,2 @@
 from .auth_errors import AuthenticationError
+from .duplicate_errors import DuplicationError
--- server/errors/auth_errors.py
+++ server/errors/auth_errors.py
@@ -1,2 +1,4 @@
 class AuthenticationError(Exception):
+    """Base class for authentication errors status_code=403"""
+
     ...
