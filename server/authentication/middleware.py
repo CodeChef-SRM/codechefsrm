11eafa0eb1e0835b43ad567ca3c8816e9841a724
Aradhya <aradhyatripathi51@gmail.com>
2021-11-25 16:54:56 +0530

    fix(minor): fixed path substitution in middleware

--- server/authentication/middleware.py
+++ server/authentication/middleware.py
@@ -10,15 +10,18 @@ import re
 
 async def verify_user(request: Request, call_next: Callable):
     path = re.sub(str(request.base_url), "", str(request.url))
-    _allowed_routes = ["/api/admin/register", "/api/admin/login"]
-    if "/api/admin" in path and path not in _allowed_routes:
+    _allowed_routes = ["api/admin/register", "api/admin/login"]
+
+    if "api/admin" in path and path not in _allowed_routes:
         try:
             token_details = get_token(request.headers)
             token = validate_token_type(token_details, "Bearer")
         except (ValueError, AssertionError, NoTokenFound) as e:
             return Response(str(e), status_code=403)
+
         value = tokens.verify_key(token)
         if isinstance(value, str):
             return Response(str(value), status_code=403)
         request.state.user = value
+
     return await call_next(request)
