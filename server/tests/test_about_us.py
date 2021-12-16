511d94cd8fcd2cdd6b3f3deebd680a9565f34288
Aradhya <aradhyatripathi51@gmail.com>
2021-12-17 00:06:59 +0530

    ✅ Added tests for about us

--- server/tests/test_about_us.py
+++ server/tests/test_about_us.py
@@ -1,3 +1,5 @@
+import json
+
 from .base import CustomTestClass
 
 
@@ -5,3 +7,34 @@ class TestAboutUs(CustomTestClass):
     def test_details(self):
         response = self.client.get(self.base_url + "/api/about-us")
         self.assertEqual(response.status_code, 200)
+
+    def test_admin_upload(self):
+        admin_creds = self.login_register_utils()["access_token"]
+        headers = {"content-type": "application/json"}
+        headers.update({"Authorization": "Bearer randomtoken"})
+        response = self.client.post(
+            self.base_url + "/api/admin/about-us",
+            data=json.dumps(
+                {"section_1": "some content", "section_2": "some other content"}
+            ),
+            headers=headers,
+        )
+        self.assertEqual(response.status_code, 403)
+
+        headers.update({"Authorization": f"Bearer {admin_creds}"})
+        about_us_data = {
+            "about_us": {
+                "section_1": "some content",
+                "section_2": "some other content",
+            }
+        }
+        response = self.client.post(
+            self.base_url + "/api/admin/about-us",
+            data=json.dumps(about_us_data),
+            headers=headers,
+        )
+        self.assertEqual(response.status_code, 201)
+
+        response = self.client.get(self.base_url + "/api/about-us")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [about_us_data["about_us"]])
